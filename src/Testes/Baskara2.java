5283f44dd6283ed86db1b1dc73516b7db0cfa44a
Gustavo Herrero Nunes <51753426+GustavoHerreroNunes@users.noreply.github.com>
2019-06-13 16:15:55 -0300

    Update Baskara2.java

--- src/Testes/Baskara2.java
+++ src/Testes/Baskara2.java
@@ -8,30 +8,30 @@ public class Baskara2 {
 		return ((Math.pow(B, 2)-4*A*C));
 	}
 	public double X1(double A, double B, double C) {
-		return(-B+(Math.sqrt(((B*B)-4*A*C))))/2*A;
+		return(-B+(Math.sqrt((Math.pow(B, 2)-4*A*C))))/2*A;
 	}
 	public double X2 (double A, double B, double C) {
-		return(-B-(Math.sqrt(((B*B)-4*A*C))))/2*A;
+		return(-B-(Math.sqrt((Math.pow(B, 2)-4*A*C))))/2*A;
 	}
 	
 	
 	public static void main(String[] args) {
 
-		double A = Double.parseDouble(JOptionPane.showInputDialog(null, "Insira o valor de A em sua equação do 2ºGrau;"));
-		double B =Double.parseDouble(JOptionPane.showInputDialog(null, "Insira o valor de B em sua equação do 2ºGrau;"));
-		double C =Double.parseDouble(JOptionPane.showInputDialog(null, "Insira o valor de C em sua equação do 2ºGrau;"));
+		double A = Double.parseDouble(JOptionPane.showInputDialog(null, "Insira o valor de A em sua equaÃ§Ã£o do 2ÂºGrau;"));
+		double B =Double.parseDouble(JOptionPane.showInputDialog(null, "Insira o valor de B em sua equaÃ§Ã£o do 2ÂºGrau;"));
+		double C =Double.parseDouble(JOptionPane.showInputDialog(null, "Insira o valor de C em sua equaÃ§Ã£o do 2ÂºGrau;"));
 		
 		Baskara2 ex= new Baskara2();
 		
-		JOptionPane.showMessageDialog(null, "Esse é o valor de seu Delta");
+		JOptionPane.showMessageDialog(null, "Esse Ã© o valor de seu Delta");
 		JOptionPane.showMessageDialog(null, ex.delta(A, B, C));
 		if (ex.delta(A, B, C)>=0) {
-			JOptionPane.showMessageDialog(null, "Esse é o valor de seu X1");
+			JOptionPane.showMessageDialog(null, "Esse Ã© o valor de seu X1");
 			JOptionPane.showMessageDialog(null, ex.X1(A, B, C));
-			JOptionPane.showMessageDialog(null, "Esse é o valor de seu X2");
+			JOptionPane.showMessageDialog(null, "Esse Ã© o valor de seu X2");
 			JOptionPane.showMessageDialog(null, ex.X2(A, B, C));
 	}else {
-		JOptionPane.showMessageDialog(null, "Seu delta é menor que 0, ou seja, imnpossivel achar as raízes das funções.");
+		JOptionPane.showMessageDialog(null, "Seu delta Ã© menor que 0, ou seja, impossivel achar as raÃ­zes das funÃ§Ãµes.");
 	}
 
 }
